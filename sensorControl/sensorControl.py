2c20905e2f80b61c21018c4240252a7626653a04
KaneiGi <wei434441092@gmail.com>
2020-06-27 00:21:55 +0900

    RTC file

---
+++ sensorControl/sensorControl.py
@@ -0,0 +1,155 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- Python -*-
+
+"""
+ @file sensorControl.py
+ @brief ModuleDescription
+ @date $Date$
+
+
+"""
+import sys
+import time
+
+sys.path.append(".")
+
+# Import RTM module
+import RTC
+import OpenRTM_aist
+
+# Import Service implementation class
+# <rtc-template block="service_impl">
+
+# </rtc-template>
+
+# Import Service stub modules
+# <rtc-template block="consumer_import">
+# </rtc-template>
+
+
+# This module's spesification
+# <rtc-template block="module_spec">
+sensorcontrol_spec = ["implementation_id", "sensorControl",
+                      "type_name", "sensorControl",
+                      "description", "ModuleDescription",
+                      "version", "1.0.0",
+                      "vendor", "KaneiGi",
+                      "category", "Category",
+                      "activity_type", "STATIC",
+                      "max_instance", "1",
+                      "language", "Python",
+                      "lang_type", "SCRIPT",
+                      "conf.default.stop_distance", "300",
+
+                      "conf.__widget__.stop_distance", "text",
+
+                      "conf.__type__.stop_distance", "short",
+
+                      ""]
+
+
+# </rtc-template>
+
+##
+# @class sensorControl
+# @brief ModuleDescription
+#
+#
+class sensorControl(OpenRTM_aist.DataFlowComponentBase):
+
+    ##
+    # @brief constructor
+    # @param manager Maneger Object
+    #
+    def __init__(self, manager):
+        OpenRTM_aist.DataFlowComponentBase.__init__(self, manager)
+
+        self._d_distance_sensor = OpenRTM_aist.instantiateDataType(RTC.TimedShortSeq)
+        """
+        """
+        self._distance_sensorIn = OpenRTM_aist.InPort("distance_sensor", self._d_distance_sensor)
+        self._d_senor_control = OpenRTM_aist.instantiateDataType(RTC.TimedShort)
+        """
+        """
+        self._senor_controlOut = OpenRTM_aist.OutPort("senor_control", self._d_senor_control)
+
+        # initialize of configuration-data.
+        # <rtc-template block="init_conf_param">
+        """
+        
+         - Name:  stop_distance
+         - DefaultValue: 300
+        """
+        self._stop_distance = [300]
+
+    def onInitialize(self):
+        # Bind variables and configuration variable
+        self.bindParameter("stop_distance", self._stop_distance, "300")
+
+        # Set InPort buffers
+        self.addInPort("distance_sensor", self._distance_sensorIn)
+
+        # Set OutPort buffers
+        self.addOutPort("senor_control", self._senor_controlOut)
+
+        # Set service provider to Ports
+
+        # Set service consumers to Ports
+
+        # Set CORBA Service Ports
+        self._last_sensor_data = [0, 0, 0, 0]
+
+        return RTC.RTC_OK
+
+    def onActivated(self, ec_id):
+        return RTC.RTC_OK
+
+    def onDeactivated(self, ec_id):
+        self._d_senor_control.data = 1
+        OpenRTM_aist.setTimestamp(self._d_senor_control)
+        self._senor_controlOut.write()
+
+        return RTC.RTC_OK
+
+    def onExecute(self, ec_id):
+        if self._distance_sensorIn.isNew():
+            data = self._distance_sensorIn.read()
+            if len(data.data) == 4:
+                self._last_sensor_data = data.data[:]
+        for d in self._last_sensor_data:
+            if d > self._stop_distance[0]:
+                self._d_senor_control.data = 0
+                OpenRTM_aist.setTimestamp(self._d_senor_control)
+                self._senor_controlOut.write()
+                return RTC.RTC_OK
+        self._d_senor_control.data = 1
+        OpenRTM_aist.setTimestamp(self._d_senor_control)
+        self._senor_controlOut.write()
+        # print(self._d_senor_control)
+        return RTC.RTC_OK
+
+
+def sensorControlInit(manager):
+    profile = OpenRTM_aist.Properties(defaults_str=sensorcontrol_spec)
+    manager.registerFactory(profile,
+                            sensorControl,
+                            OpenRTM_aist.Delete)
+
+
+def MyModuleInit(manager):
+    sensorControlInit(manager)
+
+    # Create a component
+    comp = manager.createComponent("sensorControl")
+
+
+def main():
+    mgr = OpenRTM_aist.Manager.init(sys.argv)
+    mgr.setModuleInitProc(MyModuleInit)
+    mgr.activateManager()
+    mgr.runManager()
+
+
+if __name__ == "__main__":
+    main()
---
+++ sensorControl/test/sensorControlTest.py
@@ -0,0 +1,294 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- Python -*-
+
+"""
+ @file sensorControlTest.py
+ @brief ModuleDescription
+ @date $Date$
+
+
+"""
+import sys
+import time
+sys.path.append(".")
+
+# Import RTM module
+import RTC
+import OpenRTM_aist
+
+
+# Import Service implementation class
+# <rtc-template block="service_impl">
+
+# </rtc-template>
+
+# Import Service stub modules
+# <rtc-template block="consumer_import">
+# </rtc-template>
+
+
+# This module's spesification
+# <rtc-template block="module_spec">
+sensorcontroltest_spec = ["implementation_id", "sensorControlTest", 
+		 "type_name",         "sensorControlTest", 
+		 "description",       "ModuleDescription", 
+		 "version",           "1.0.0", 
+		 "vendor",            "KaneiGi", 
+		 "category",          "Category", 
+		 "activity_type",     "STATIC", 
+		 "max_instance",      "1", 
+		 "language",          "Python", 
+		 "lang_type",         "SCRIPT",
+		 "conf.default.stop_distance", "300",
+
+		 "conf.__widget__.stop_distance", "text",
+
+         "conf.__type__.stop_distance", "short",
+
+		 ""]
+# </rtc-template>
+
+##
+# @class sensorControlTest
+# @brief ModuleDescription
+# 
+# 
+class sensorControlTest(OpenRTM_aist.DataFlowComponentBase):
+	
+	##
+	# @brief constructor
+	# @param manager Maneger Object
+	# 
+	def __init__(self, manager):
+		OpenRTM_aist.DataFlowComponentBase.__init__(self, manager)
+
+		self._d_senor_control = OpenRTM_aist.instantiateDataType(RTC.TimedShort)
+		"""
+		"""
+		self._senor_controlIn = OpenRTM_aist.InPort("senor_control", self._d_senor_control)
+		self._d_distance_sensor = OpenRTM_aist.instantiateDataType(RTC.TimedShortSeq)
+		"""
+		"""
+		self._distance_sensorOut = OpenRTM_aist.OutPort("distance_sensor", self._d_distance_sensor)
+
+
+		
+
+
+		# initialize of configuration-data.
+		# <rtc-template block="init_conf_param">
+		"""
+		
+		 - Name:  stop_distance
+		 - DefaultValue: 300
+		"""
+		self._stop_distance = [300]
+		
+		# </rtc-template>
+
+
+		 
+	##
+	#
+	# The initialize action (on CREATED->ALIVE transition)
+	# formaer rtc_init_entry() 
+	# 
+	# @return RTC::ReturnCode_t
+	# 
+	#
+	def onInitialize(self):
+		# Bind variables and configuration variable
+		self.bindParameter("stop_distance", self._stop_distance, "300")
+		
+		# Set InPort buffers
+		self.addInPort("senor_control",self._senor_controlIn)
+		
+		# Set OutPort buffers
+		self.addOutPort("distance_sensor",self._distance_sensorOut)
+		
+		# Set service provider to Ports
+		
+		# Set service consumers to Ports
+		
+		# Set CORBA Service Ports
+		
+		return RTC.RTC_OK
+	
+	#	##
+	#	# 
+	#	# The finalize action (on ALIVE->END transition)
+	#	# formaer rtc_exiting_entry()
+	#	# 
+	#	# @return RTC::ReturnCode_t
+	#
+	#	# 
+	#def onFinalize(self):
+	#
+	#	return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The startup action when ExecutionContext startup
+	#	# former rtc_starting_entry()
+	#	# 
+	#	# @param ec_id target ExecutionContext Id
+	#	#
+	#	# @return RTC::ReturnCode_t
+	#	#
+	#	#
+	#def onStartup(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The shutdown action when ExecutionContext stop
+	#	# former rtc_stopping_entry()
+	#	#
+	#	# @param ec_id target ExecutionContext Id
+	#	#
+	#	# @return RTC::ReturnCode_t
+	#	#
+	#	#
+	#def onShutdown(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+		##
+		#
+		# The activated action (Active state entry action)
+		# former rtc_active_entry()
+		#
+		# @param ec_id target ExecutionContext Id
+		# 
+		# @return RTC::ReturnCode_t
+		#
+		#
+	def onActivated(self, ec_id):
+	
+		return RTC.RTC_OK
+	
+		##
+		#
+		# The deactivated action (Active state exit action)
+		# former rtc_active_exit()
+		#
+		# @param ec_id target ExecutionContext Id
+		#
+		# @return RTC::ReturnCode_t
+		#
+		#
+	def onDeactivated(self, ec_id):
+	
+		return RTC.RTC_OK
+	
+		##
+		#
+		# The execution action that is invoked periodically
+		# former rtc_active_do()
+		#
+		# @param ec_id target ExecutionContext Id
+		#
+		# @return RTC::ReturnCode_t
+		#
+		#
+	def onExecute(self, ec_id):
+	
+		return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The aborting action when main logic error occurred.
+	#	# former rtc_aborting_entry()
+	#	#
+	#	# @param ec_id target ExecutionContext Id
+	#	#
+	#	# @return RTC::ReturnCode_t
+	#	#
+	#	#
+	#def onAborting(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The error action in ERROR state
+	#	# former rtc_error_do()
+	#	#
+	#	# @param ec_id target ExecutionContext Id
+	#	#
+	#	# @return RTC::ReturnCode_t
+	#	#
+	#	#
+	#def onError(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The reset action that is invoked resetting
+	#	# This is same but different the former rtc_init_entry()
+	#	#
+	#	# @param ec_id target ExecutionContext Id
+	#	#
+	#	# @return RTC::ReturnCode_t
+	#	#
+	#	#
+	#def onReset(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The state update action that is invoked after onExecute() action
+	#	# no corresponding operation exists in OpenRTm-aist-0.2.0
+	#	#
+	#	# @param ec_id target ExecutionContext Id
+	#	#
+	#	# @return RTC::ReturnCode_t
+	#	#
+
+	#	#
+	#def onStateUpdate(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The action that is invoked when execution context's rate is changed
+	#	# no corresponding operation exists in OpenRTm-aist-0.2.0
+	#	#
+	#	# @param ec_id target ExecutionContext Id
+	#	#
+	#	# @return RTC::ReturnCode_t
+	#	#
+	#	#
+	#def onRateChanged(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+
+
+
+def sensorControlTestInit(manager):
+    profile = OpenRTM_aist.Properties(defaults_str=sensorcontroltest_spec)
+    manager.registerFactory(profile,
+                            sensorControlTest,
+                            OpenRTM_aist.Delete)
+
+def MyModuleInit(manager):
+    sensorControlTestInit(manager)
+
+    # Create a component
+    comp = manager.createComponent("sensorControlTest")
+
+def main():
+	mgr = OpenRTM_aist.Manager.init(sys.argv)
+	mgr.setModuleInitProc(MyModuleInit)
+	mgr.activateManager()
+	mgr.runManager()
+
+if __name__ == "__main__":
+	main()
+
